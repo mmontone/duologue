9b1bc4ce3c577f33d4e1aca4ce24f06ed7768f80
Mariano Montone <marianomontone@gmail.com>
2015-03-22 02:29:24 -0300

    More documentation

--- README.md
+++ README.md
@@ -37,6 +37,45 @@ Example:
 **Tags**: menu, choose
 
 
+### choose-many (msg options &key if-wrong-option default (print-options t) (separator ~%) complete (test (function eql)) (color \*prompt-color\*) (error-color \*prompt-error-color\*))
+Asks the user to choose many of the given options.
+
+- **msg**: (string) The prompt message.
+- **options**: (list) The list of options the user can choose from.
+- **if-wrong-option**: (function) When present, this function is run if the user enters a wrong option. Default: nil.
+- **default**: The default value. The default value is selected if the user just hits the ENTER key. Default: nil.
+- **print-options**: (boolean) Print the options on the screen. Default: T.
+- **separator**: (string) Separation string to use when printing the options. Default: '~%'
+- **complete**: If T, then readline completion is enabled. Default: nil.
+- **color**: Color to use at prompt. Default: *prompt-color*
+- **error-color**: Color to use when error ocurrs. Default: *prompt-error-color*
+
+
+Example: 
+
+  ```
+(choose-many "Choose: " (list "foo" "bar" "baz") :default "baz")
+```
+**Tags**: menu, choose
+
+
+### parse-prompt (parser &optional msg &key default (required-p t) validator if-invalid (color \*prompt-color\*) (error-color \*prompt-error-color\*))
+Like prompt, but parses its input.
+
+- **parser**: A function that parses the input string. Should return NIL if it cannot parse.
+- **msg**: The prompt.
+- **default**: Default value. This is returned if the user enters the empty string. Default: nil.
+- **required-p**: (boolean) If T, then the empty string is not allowed as a valid input, and the user is asked again for input. Default: t.
+- **validator**: (function) A function to use to validate the input. Should return T if the input is valid, or NIL otherwise.
+- **if-invalid**: (function) Function to execute if the validator fails.
+- **color**: Prompt color
+- **error-color**: Prompt error color.
+
+
+**Returns**: The parsed value or the default value, depending on what the user entered
+
+
+
 ### prompt (&optional msg &key default (required-p t) validator if-invalid (color \*prompt-color\*) (error-color \*prompt-error-color\*))
 Prompt for a string.
 
@@ -48,6 +87,96 @@ Prompt for a string.
 - **color**: Prompt color
 - **error-color**: Prompt error color.
 
+
+
+
+### prompt-datetime (&optional msg &key default (required-p t) if-invalid (color \*prompt-color\*) (error-color \*prompt-error-color\*))
+Prompts for a timestamp.
+
+- **msg**: The prompt.
+- **default**: Default value. This is returned if the user enters the empty string. Default: nil.
+- **required-p**: (boolean) If T, then the empty string is not allowed as a valid input, and the user is asked again for input. Default: t.
+- **if-invalid**: (function) Function to execute if the validator fails.
+- **color**: Prompt color
+- **error-color**: Prompt error color.
+
+
+The input is parsed with chronicity library and transformed to a local-time. 
+   The input is validated and the process does not stop until the user enters a valid timestamp address.
+
+   Return: the parsed local-time
+
+### prompt-email (&optional msg &key default (required-p t) if-invalid (color \*prompt-color\*) (error-color \*prompt-error-color\*))
+Prompts for an email.
+
+- **msg**: The prompt.
+- **default**: Default value. This is returned if the user enters the empty string. Default: nil.
+- **required-p**: (boolean) If T, then the empty string is not allowed as a valid input, and the user is asked again for input. Default: t.
+- **if-invalid**: (function) Function to execute if the validator fails.
+- **color**: Prompt color
+- **error-color**: Prompt error color.
+
+
+The email is validated and the process does not stop until the user enters a valid email address.
+
+   Return: the entered email
+
+### prompt-integer (&optional msg &key default (required-p t) if-invalid (color \*prompt-color\*) (error-color \*prompt-error-color\*))
+Prompts for an integer.
+
+- **msg**: The prompt.
+- **default**: Default value. This is returned if the user enters the empty string. Default: nil.
+- **required-p**: (boolean) If T, then the empty string is not allowed as a valid input, and the user is asked again for input. Default: t.
+- **if-invalid**: (function) Function to execute if the validator fails.
+- **color**: Prompt color
+- **error-color**: Prompt error color.
+
+
+**Returns**: the entered number
+
+
+
+### prompt-pathname (&optional msg &key default (required-p t) if-invalid (color \*prompt-color\*) (error-color \*prompt-error-color\*) probe if-exists (if-does-not-exist error) absolute-p file-type directory-p)
+Prompts for a pathname.
+
+- **msg**: The prompt.
+- **default**: Default value. This is returned if the user enters the empty string. Default: nil.
+- **required-p**: (boolean) If T, then the empty string is not allowed as a valid input, and the user is asked again for input. Default: t.
+- **if-invalid**: (function) Function to execute if the validator fails.
+- **color**: Prompt color
+- **error-color**: Prompt error color.
+- probe. If T, checks that the file exists on the filesystem.
+- **if-exists**: Function to call if the probe is successful.
+- **if-does-not-exist**: (keyword) One of:
+* :error : Tries again until the pathname can be accessed.
+* :warn : Warns the user the pathname could not be accessed and asks for continuing.
+* :warn-and-continue: Warns the user the pathname could not be accessed and continues.
+* :warn-and-ask-again: Warns the user the pathname could not be accessed and asks for another pathname.
+
+
+
+
+
+### prompt-url (&optional msg &key default (required-p t) if-invalid (color \*prompt-color\*) (error-color \*prompt-error-color\*) probe if-exists (if-does-not-exist error))
+Prompts for an url.
+
+- **msg**: The prompt.
+- **default**: Default value. This is returned if the user enters the empty string. Default: nil.
+- **required-p**: (boolean) If T, then the empty string is not allowed as a valid input, and the user is asked again for input. Default: t.
+- **if-invalid**: (function) Function to execute if the validator fails.
+- **color**: Prompt color
+- **error-color**: Prompt error color
+- **probe**: (boolean) If T, then url is accessed and verified.
+- **if-exists**: (function) A function to call if the url exists (can be accessed).
+- **if-does-not-exist**: (keyword) One of:
+* :error : Tries again until the url can be accessed.
+* :warn : Warns the user the url could not be accessed and asks for continuing.
+* :warn-and-continue: Warns the user the url could not be accessed and continues.
+* :warn-and-ask-again: Warns the user the url could not be accessed and asks for another url.
+
+
+
+**Returns**: the entered url
 
 
 
--- duologue.lisp
+++ duologue.lisp
@@ -220,6 +220,18 @@
 					    if-invalid
 					    (color *prompt-color*)
 					    (error-color *prompt-error-color*))
+  "Like prompt, but parses its input.
+
+   Args: - parser: A function that parses the input string. Should return NIL if it cannot parse.
+         - msg: The prompt.
+         - default: Default value. This is returned if the user enters the empty string. Default: nil.
+         - required-p(boolean): If T, then the empty string is not allowed as a valid input, and the user is asked again for input. Default: t.
+         - validator(function): A function to use to validate the input. Should return T if the input is valid, or NIL otherwise.
+         - if-invalid(function): Function to execute if the validator fails.
+         - color: Prompt color
+         - error-color: Prompt error color.
+
+  Returns: The parsed value or the default value, depending on what the user entered"
   (loop do
        (when msg
 	 (say msg :color color))
@@ -243,13 +255,23 @@
 		    (funcall if-invalid)
 		    (say "Invalid value" :color error-color)))
 	       (parsed-input
-		(return parsed-input))))))	       
+		(return parsed-input))))))
 
 (defun prompt-integer (&optional msg &key default 
 				       (required-p t) 
 				       if-invalid
 				       (color *prompt-color*)
 				       (error-color *prompt-error-color*))
+  "Prompts for an integer.
+
+   Args: - msg: The prompt.
+         - default: Default value. This is returned if the user enters the empty string. Default: nil.
+         - required-p(boolean): If T, then the empty string is not allowed as a valid input, and the user is asked again for input. Default: t.
+         - if-invalid(function): Function to execute if the validator fails.
+         - color: Prompt color
+         - error-color: Prompt error color.
+
+   Returns: the entered number"
   (parse-prompt #'parse-integer msg 
 		:default default
 		:required-p required-p
@@ -263,6 +285,18 @@
 				     if-invalid
 				     (color *prompt-color*)
 				     (error-color *prompt-error-color*))
+  "Prompts for an email.
+
+   Args: - msg: The prompt.
+         - default: Default value. This is returned if the user enters the empty string. Default: nil.
+         - required-p(boolean): If T, then the empty string is not allowed as a valid input, and the user is asked again for input. Default: t.
+         - if-invalid(function): Function to execute if the validator fails.
+         - color: Prompt color
+         - error-color: Prompt error color.
+
+   The email is validated and the process does not stop until the user enters a valid email address.
+
+   Return: the entered email"
   (prompt msg :default default
 	  :required-p required-p
 	  :validator (clavier:valid-email)
@@ -280,6 +314,23 @@
 				   probe
 				   if-exists
 				   (if-does-not-exist :error))
+  "Prompts for an url.
+
+   Args: - msg: The prompt.
+         - default: Default value. This is returned if the user enters the empty string. Default: nil.
+         - required-p(boolean): If T, then the empty string is not allowed as a valid input, and the user is asked again for input. Default: t.
+         - if-invalid(function): Function to execute if the validator fails.
+         - color: Prompt color
+         - error-color: Prompt error color
+         - probe(boolean): If T, then url is accessed and verified.
+         - if-exists(function): A function to call if the url exists (can be accessed).
+         - if-does-not-exist(keyword): One of:
+              * :error : Tries again until the url can be accessed.
+              * :warn : Warns the user the url could not be accessed and asks for continuing.
+              * :warn-and-continue: Warns the user the url could not be accessed and continues.
+              * :warn-and-ask-again: Warns the user the url could not be accessed and asks for another url.
+
+   Returns: the entered url"
   (flet ((recurse ()
 	   (return-from prompt-url
 	     (prompt-url msg :default default
@@ -332,6 +383,21 @@
 					absolute-p
 					file-type
 					directory-p)
+  "Prompts for a pathname.
+
+   Args: - msg: The prompt.
+         - default: Default value. This is returned if the user enters the empty string. Default: nil.
+         - required-p(boolean): If T, then the empty string is not allowed as a valid input, and the user is asked again for input. Default: t.
+         - if-invalid(function): Function to execute if the validator fails.
+         - color: Prompt color
+         - error-color: Prompt error color.
+         - probe. If T, checks that the file exists on the filesystem.
+         - if-exists: Function to call if the probe is successful.
+         - if-does-not-exist(keyword): One of:
+              * :error : Tries again until the pathname can be accessed.
+              * :warn : Warns the user the pathname could not be accessed and asks for continuing.
+              * :warn-and-continue: Warns the user the pathname could not be accessed and continues.
+              * :warn-and-ask-again: Warns the user the pathname could not be accessed and asks for another pathname."
   (flet ((recurse ()
 	   (return-from prompt-pathname
 	     (prompt-pathname msg :default default
@@ -382,6 +448,20 @@
 					if-invalid
 					(color *prompt-color*)
 					(error-color *prompt-error-color*))
+  "Prompts for a timestamp.
+
+   Args: - msg: The prompt.
+         - default: Default value. This is returned if the user enters the empty string. Default: nil.
+         - required-p(boolean): If T, then the empty string is not allowed as a valid input, and the user is asked again for input. Default: t.
+         - if-invalid(function): Function to execute if the validator fails.
+         - color: Prompt color
+         - error-color: Prompt error color.
+
+   The input is parsed with chronicity library and transformed to a local-time. 
+   The input is validated and the process does not stop until the user enters a valid timestamp address.
+
+   Return: the parsed local-time"
+
   (parse-prompt #'chronicity:parse msg
 		:default default
 		:required-p required-p
@@ -399,6 +479,23 @@
 				  (test #'eql)
 				  (color *prompt-color*)
 				  (error-color *prompt-error-color*))
+  "Asks the user to choose many of the given options.
+
+   Args: - msg(string): The prompt message.
+         - options(list): The list of options the user can choose from.
+         - if-wrong-option(function): When present, this function is run if the user enters a wrong option. Default: nil.
+         - default: The default value. The default value is selected if the user just hits the ENTER key. Default: nil.
+         - print-options(boolean): Print the options on the screen. Default: T.
+         - separator(string): Separation string to use when printing the options. Default: '~%'
+         - complete: If T, then readline completion is enabled. Default: nil.
+         - color: Color to use at prompt. Default: *prompt-color*
+         - error-color: Color to use when error ocurrs. Default: *prompt-error-color*
+
+  Example: 
+
+  ``(choose-many \"Choose: \" (list \"foo\" \"bar\" \"baz\") :default \"baz\")``
+
+  Tags: menu, choose" 
   (let ((chosen-options nil))
     (flet ((print-options ()
 	     (loop 
